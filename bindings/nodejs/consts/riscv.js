2578bc29d4f9f3a10f81ab33e7693e874be5263b
Etto48 <ettore.ricci.coding@gmail.com>
2025-04-30 18:41:42 +0200

    Removed bindings

--- bindings/nodejs/consts/riscv.js
+++
@@ -1,4 +0,0 @@
-// For Keystone Engine. AUTO-GENERATED FILE, DO NOT EDIT [riscv_const.js]
-module.exports.ERR_ASM_RISCV_INVALIDOPERAND = 512
-module.exports.ERR_ASM_RISCV_MISSINGFEATURE = 513
-module.exports.ERR_ASM_RISCV_MNEMONICFAIL = 514
